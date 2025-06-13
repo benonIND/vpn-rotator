0f11066b428e7a93a6a36f33b254c341eab8e06d
Yorima_ <56250907+benonIND@users.noreply.github.com>
2025-06-13 17:59:09 +0700

    Update main.py
    Update

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
-from gps_spoofer import set_mock_location, generate_random_location
-from ip_changer import change_tor_ip, get_ip_info
+#!/data/data/com.termux/files/usr/bin/python3
 
-def start_ip_and_location_rotation(interval=10, change_gps=True):
-    """Ganti IP + GPS secara periodik"""
-    def rotation_thread():
-        while True:
-            # Dapatkan info IP baru
-            change_tor_ip()
-            ip_info = get_ip_info()
-            
-            # Update GPS sesuai lokasi IP
-            if change_gps and ip_info['lat']:
-                set_mock_location(ip_info['lat'], ip_info['lon'])
-            
-            print(f"""
-\033[1;36m=== Rotasi Berhasil ===
-IP: {ip_info['ip']}
-Lokasi: {ip_info['city']}, {ip_info['country']}
-Koordinat: {ip_info['lat']}, {ip_info['lon']}
-\033[0m""")
-            
-            time.sleep(interval)
+import sys
+import os
+import time
+from banner import show_banner, show_status
+from ip_changer import IPChanger
+from dns_adblock import DNSManager
+
+def setup_environment():
+    os.system('clear')
+    if not os.path.exists('/data/data/com.termux/files/usr/bin/tor'):
+        print("\033[1;31m[!] Tor belum terinstall! Jalankan:")
+        print("pkg install tor -y")
+        print("pip install stem requests\033[0m")
+        sys.exit(1)
+
+def main():
+    setup_environment()
+    show_banner()
     
-    threading.Thread(target=rotation_thread, daemon=True).start()
+    # Init modules
+    ip_changer = IPChanger()
+    dns_manager = DNSManager()
+    
+    try:
+        # Set default DNS
+        dns_manager.set_dns('adguard')
+        
+        # Start IP rotation
+        ip_changer.start(interval=10)
+        
+        # Show initial status
+        initial_ip = ip_changer.get_ip_info()
+        show_status(initial_ip['ip'], initial_ip['country'], dns_manager.current_dns)
+        
+        # Keep alive
+        input("\nTekan Enter untuk berhenti...\n")
+        
+    except KeyboardInterrupt:
+        pass
+    finally:
+        ip_changer.stop()
+        print("\n\033[1;31m[!] Service dihentikan\033[0m")
+
+if __name__ == "__main__":
+    main()
