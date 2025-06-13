068828ed1225069e8409eec9948629782756647d
Yorima_ <56250907+benonIND@users.noreply.github.com>
2025-06-14 00:55:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -49,12 +49,15 @@ def main():
                 except:
                     print("\033[1;31m[!] Input tidak valid\033[0m")
             elif choice == '2':
-                interval = int(input("Interval (detik): "))
-                    if not int(interval) :
-                        print("\033[1;31m[!] Masukkan angka\033[0m")
-                    else:
+                try :
+                    interval = int(input("Interval (detik): "))
+                    if interval >= 1:
                         print("\033[1;33m[!] Auto-rotate mulai...\033[0m")
                         vpn.auto_rotate(interval)
+                    else :
+                        print("\033[1;31m[!] Masukkan angka\033[0m")
+                except:
+                        sys.exit(0)
                 
             elif choice == '3':
                 print("\n\033[1;34mDNS SERVERS:\033[0m")
