87cc5e6f9ca5ca36ca65d0c1dda53c1ce12ed954
Yorima_ <56250907+benonIND@users.noreply.github.com>
2025-06-13 20:40:33 +0700

    Update banner.py

--- banner.py
+++ banner.py
@@ -15,10 +15,11 @@ def show_banner():
   \033[1;31m⚠ Gunakan Secara Bertanggung Jawab ⚠
   \033[0m""")
 
-def show_status(ip, country, dns_server):
+def show_status(vpn_status, ip_info):
     print(f"""\033[1;35m
-  STATUS:
-  IP: {ip}
-  Lokasi: {country}
-  DNS: {dns_server}
-  \033[0m""")
+      STATUS:
+      VPN: {vpn_status['server'] or 'Not Connected'}
+      IP: {ip_info['ip']}
+      Location: {ip_info['country']} - {ip_info['city']}
+      ISP: {ip_info['isp']}
+      \033[0m""")
